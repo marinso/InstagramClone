2630a20c176061a2c0611b955dfaf4d612513e23
Martin Nasierowski <mnasierowski@netkodo.com>
2019-09-13 13:07:25 +0200

    Initial Commit

---
+++ InstagramClone/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  InstagramClone
+//
+//  Created by Martin Nasierowski on 13/09/2019.
+//  Copyright © 2019 Martin Nasierowski. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
