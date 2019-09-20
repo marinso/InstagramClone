62ece34c2c6ce2b36ad0c18d2556af90a4024954
Martin Nasierowski <mnasierowski@netkodo.com>
2019-09-20 15:14:40 +0200

    Create searchprofileCell and set in SearchVC

--- InstagramClone/Controller/Search/ss.swift
+++ InstagramClone/Controller/Search/ss.swift
@@ -1,8 +1,8 @@
 //
-//  TableViewController.swift
+//  SearchVC.swift
 //  InstagramClone
 //
-//  Created by Martin Nasierowski on 20/09/2019.
+//  Created by Martin Nasierowski on 19/09/2019.
 //  Copyright © 2019 Martin Nasierowski. All rights reserved.
 //
 
@@ -14,17 +14,22 @@ class SearchVC: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+
         tableView.register(SearchUserCell.self, forCellReuseIdentifier: reuseIdentifier)
         tableView.separatorInset = UIEdgeInsets(top: 0, left: 64, bottom: 0, right: 0)
         configureNavController()
-        
     }
 
     // MARK: - Table view data source
-
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: reuseIdentifier, for: indexPath) as! SearchUserCell
+        print("ABC")
+        return cell
+    }
+    
     override func numberOfSections(in tableView: UITableView) -> Int {
-        return 1
+        return 0
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -32,17 +37,10 @@ class SearchVC: UITableViewController {
     }
     
     override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 80
-    }
-    
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: reuseIdentifier, for: indexPath) as! SearchUserCell
-     
-        return cell
+        return 60
     }
     
     func configureNavController() {
         navigationItem.title = "Explore"
     }
-
 }
---
+++ InstagramClone/View/SearchUserCell.swift
@@ -0,0 +1,64 @@
+//
+//  SearchUserCell.swift
+//  InstagramClone
+//
+//  Created by Martin Nasierowski on 20/09/2019.
+//  Copyright © 2019 Martin Nasierowski. All rights reserved.
+//
+
+import UIKit
+
+class SearchUserCell: UITableViewCell {
+    
+    let profileImageView: UIImageView = {
+        let iv = UIImageView()
+        iv.backgroundColor = .lightGray
+        iv.contentMode = .scaleAspectFill
+        iv.clipsToBounds = true
+        return iv
+    }()
+    
+    let usernameLabel: UILabel = {
+        var label = UILabel()
+        label.textColor = UIColor.black
+        label.font = UIFont.boldSystemFont(ofSize: 12)
+        label.text = "Username"
+        label.textAlignment = .left
+        return label
+    }()
+    
+    let fullNameLabel: UILabel = {
+        var label = UILabel()
+        label.textColor = UIColor.lightGray
+        label.font = UIFont.systemFont(ofSize: 12)
+        label.text = "Full name"
+        label.textAlignment = .left
+        return label
+    }()
+
+
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        addSubview(profileImageView)
+        profileImageView.anchor(top: nil, bottom: nil, left: leftAnchor, right: nil, paddingTop: 0, paddingBottom: 0, paddingLeft: 8, paddingRight: 0, width: 48, height: 48)
+        profileImageView.centerYAnchor.constraint(equalTo: centerYAnchor).isActive = true
+        profileImageView.layer.cornerRadius = 48 / 2
+        profileImageView.clipsToBounds = true
+        
+        addSubview(usernameLabel)
+        usernameLabel.translatesAutoresizingMaskIntoConstraints = false
+        usernameLabel.centerXAnchor.constraint(equalTo: profileImageView.rightAnchor, constant: 48).isActive = true
+        usernameLabel.centerYAnchor.constraint(equalTo: centerYAnchor, constant: -10).isActive = true
+        
+        addSubview(fullNameLabel)
+        fullNameLabel.translatesAutoresizingMaskIntoConstraints = false
+        fullNameLabel.centerXAnchor.constraint(equalTo: profileImageView.rightAnchor, constant: 46 ).isActive = true
+        fullNameLabel.centerYAnchor.constraint(equalTo: centerYAnchor, constant: 10).isActive = true
+        
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
